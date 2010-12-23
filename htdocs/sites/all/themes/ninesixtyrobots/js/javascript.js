d48b7ea1b4a5dc82609c997daed0d8888e19bede
joe <eojthebrave@gmail.com>
2010-12-23 10:18:47 -0600

    Convert to D7 style javascript w/ Drupal.bahviors.
    Fixes autofill on search form.

--- htdocs/sites/all/themes/ninesixtyrobots/js/javascript.js
+++ htdocs/sites/all/themes/ninesixtyrobots/js/javascript.js
@@ -1,8 +1,14 @@
 // $Id: javascript.js,v 1.1.4.2 2009/08/12 23:29:56 add1sun Exp $
 
 // Prefill the search box with Search... text.
-$(document).ready(function(){
-  $('#search input:text').autofill({
-    value: "Search..."
-  });
-});
+(function ($) {
+
+Drupal.behaviors.ninesixtyrobots = {
+  attach: function (context) {
+    $('#search-block-form input:text', context).autofill({
+      value: "Search ..."
+    });
+  }
+};
+
+})(jQuery);
