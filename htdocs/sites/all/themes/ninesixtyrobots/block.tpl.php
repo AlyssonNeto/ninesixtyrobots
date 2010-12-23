9840fc5e7bd41a889e745e0483442809ebea4dda
joe <eojthebrave@gmail.com>
2010-12-23 08:39:47 -0600

    Replace contents of block.tpl.php with template file from block module. Change <h2> to <h3> to match old version.

--- htdocs/sites/all/themes/ninesixtyrobots/block.tpl.php
+++ htdocs/sites/all/themes/ninesixtyrobots/block.tpl.php
@@ -1,12 +1,15 @@
 <?php
 // $Id: block.tpl.php,v 1.1.4.1 2009/08/05 18:55:54 add1sun Exp $
 ?>
-<div id="block-<?php print $block->module .'-'. $block->delta; ?>" class="block block-<?php print $block->module ?>">
-<?php if ($block->subject): ?>
-  <h3 class="title"><?php print $block->subject ?></h3>
-<?php endif;?>
+<div id="<?php print $block_html_id; ?>" class="<?php print $classes; ?>"<?php print $attributes; ?>>
 
-  <div class="content">
-    <?php print $block->content ?>
+  <?php print render($title_prefix); ?>
+<?php if ($block->subject): ?>
+  <h3<?php print $title_attributes; ?>><?php print $block->subject ?></h3>
+<?php endif;?>
+  <?php print render($title_suffix); ?>
+
+  <div class="content"<?php print $content_attributes; ?>>
+    <?php print $content ?>
   </div>
 </div>
