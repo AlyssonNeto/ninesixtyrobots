dd7a0d83ed368cb9eb0b6c5b7193ea23f52cf988
joe <eojthebrave@gmail.com>
2010-12-23 08:20:03 -0600

    Change  to  in node.tpl.php

--- htdocs/sites/all/themes/ninesixtyrobots/node.tpl.php
+++ htdocs/sites/all/themes/ninesixtyrobots/node.tpl.php
@@ -50,7 +50,7 @@
 ?>
 <div id="node-<?php print $node->nid; ?>" class="node post<?php if ($sticky) { print ' sticky'; } ?><?php if (!$status) { print ' node-unpublished'; } ?> clear-block">
 
-<?php print $picture ?>
+<?php print $user_picture ?>
 
 <?php if (!$page): ?>
   <h2><a href="<?php print $node_url ?>" title="<?php print $title ?>"><?php print $title ?></a></h2>
