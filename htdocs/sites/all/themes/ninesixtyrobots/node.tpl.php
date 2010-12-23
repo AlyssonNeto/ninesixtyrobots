66719255f44114cd012566aeb890ff49cd2b1b40
joe <eojthebrave@gmail.com>
2010-12-23 08:24:11 -0600

    Update <h2> title section of node.tpl.php. Add  to <h2> and render()  and .

--- htdocs/sites/all/themes/ninesixtyrobots/node.tpl.php
+++ htdocs/sites/all/themes/ninesixtyrobots/node.tpl.php
@@ -52,9 +52,11 @@
 
 <?php print $user_picture ?>
 
+<?php print render($title_prefix); ?>
 <?php if (!$page): ?>
-  <h2><a href="<?php print $node_url ?>" title="<?php print $title ?>"><?php print $title ?></a></h2>
+  <h2<?php print $title_attributes; ?>><a href="<?php print $node_url; ?>"><?php print $title; ?></a></h2>
 <?php endif; ?>
+<?php print render($title_suffix); ?>
 
   <div class="meta post-info">
   <?php if ($display_submitted): ?>
